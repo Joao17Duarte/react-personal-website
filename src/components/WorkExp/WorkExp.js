0be25e15b4649d512a7d49febd5d757983a23a5a
Joao Duarte <joaopd17@hotmail.com>
2021-05-06 17:25:02 +0200

    Add workexp component and work and school svgs

---
+++ src/components/WorkExp/WorkExp.js
@@ -0,0 +1,24 @@
+import styled from "styled-components/macro";
+
+
+export default function WorkExp() {
+  return (
+    <div>
+          
+      Work Experience 
+
+      <ul>
+        <li>2021</li>
+        <p>neuefische bootcamp</p>
+        <li>2020</li>
+        <p>Spa-Therapist</p>
+        <li>2019</li>
+        <p>Spa-Therapist</p>
+        <li>2018</li>
+        <p>Spa-Therapist</p>
+        <li>2017</li>
+        <p>Student</p>
+      </ul>
+    </div>
+  )
+}
