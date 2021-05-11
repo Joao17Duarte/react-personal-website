097f150d7e50356ed5423412d22813763648fbe9
Joao Duarte <joaopd17@hotmail.com>
2021-05-11 18:39:54 +0200

    Remove assets from public to src folder and add dummy pics for project carousel

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -3,7 +3,9 @@ import styled from 'styled-components/macro'
 import { ProjectData } from './ProjectData'
 import { FaArrowRight, FaArrowLeft } from 'react-icons/fa'
 
-export default function Card({ slides }) {
+// import Hero from '../../assets/profile.jpg'
+
+export default function Projects({ slides }) {
   const [current, setCurrent] = useState(0)
   const length = slides.length
 
