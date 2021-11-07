70ea3c810114d7772fac860609be08e2bab72f98
Joao Duarte <joaopd17@hotmail.com>
2021-11-07 16:43:42 +0100

    Remove comments

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ export default function App() {
         
         <CodeLanguage />
 
-        {/* <Projects slides={ProjectData} /> */}
         <Project slides={ProjectData} />
         
         <WorkExp />
